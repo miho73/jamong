e9e269281ef6b0863b8f3de3a602778fbed017bb
miho73 <hyrexfiels@gmail.com>
2021-05-26 01:41:28 +0900

    Made project

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const favicon = require('serve-favicon');
+const https = require('https');
+const fs = require('fs');
+
+const HTTP_PORT = 8800
+const HTTPS_PORT = 4400
+
+const app = express();
+//app.use(favicon(__dirname + '/library/resources/favicon.ico'));
+app.set("view engine", "ejs");
+app.use('/lib', express.static('./lib'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=>{
+    res.render('main.ejs');
+});
+
+app.listen(HTTP_PORT);
+console.log("HTTP server listening on port " + HTTP_PORT);
