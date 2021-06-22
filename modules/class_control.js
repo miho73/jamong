7b7e540165a3e5df2e62d16ea5ab1e0b47dbee68
miho73 <hyrexfiels@gmail.com>
2021-06-23 00:20:48 +0900

    beta complete

--- modules/class_control.js
+++ modules/class_control.js
@@ -131,9 +131,13 @@ module.exports = {
         });
         app.post('/class/treg', (req, res)=>{
             //check permission
-            let code = req.body.rc, codex = req.body.uid;
-            if(!number.test(code) || code=='' || code == undefined || codex == undefined || codex.length != 5) {
+            let code = req.body.rc, codex = req.body.uid, probCont = req.body.cont;
+            if(!number.test(code) || code=='' || code == undefined || codex == undefined || codex.length != 5 || probCont == undefined) {
                 res.sendStatus(400);
+                return;
+            }
+            if(probCont == '') {
+                res.status(400).send('blk_cont');
                 return;
             }
             let authed = function() {
@@ -156,7 +160,7 @@ module.exports = {
                     }
                     exp = exp-exp%86400+86399;
                     cmanage.clinfDbQuery(`BEGIN`);
-                    cmanage.clinfDbQuery(`INSERT INTO h${code} (content, expires) VALUES ($1, $2)`, [sanitizeHtml(req.body.cont), exp], (err1)=>{
+                    cmanage.clinfDbQuery(`INSERT INTO h${code} (content, expires) VALUES ($1, $2)`, [sanitizeHtml(probCont), exp], (err1)=>{
                         if(err1) {
                             res.status(500).send('db');
                             reject('class_control: /class/treg db error: '+err1);
@@ -171,7 +175,7 @@ module.exports = {
                                     cmanage.clinfDbQuery('ROLLBACK');
                                     return; 
                                 }
-                                cmanage.clinfDbQuery(`UPDATE u${code} SET not_done = CONCAT(not_done, '${res5.rows[0].code};')`, [], (err2)=>{
+                                cmanage.clinfDbQuery(`UPDATE u${code} SET not_done = CONCAT(not_done, ';${res5.rows[0].code}')`, [], (err2)=>{
                                     if(err2) {
                                         res.status(500).send('db');
                                         reject('class_control: /class/treg user not_done modify error: '+err2);
@@ -265,11 +269,53 @@ module.exports = {
             cmanage.clinfDbQuery(`UPDATE u${code} SET not_done=$1 WHERE uid=$2`, [req.body.stat, codex], (err1)=>{
                 if(err1) {
                     console.log('class_control: /class/clear change status failrue: '+err1);
-                    res.status(500).send("처리 실패");
+                    res.status(500).send("db");
                     return;
                 }
                 res.sendStatus(200);
             });
-        })
+        });
+        app.post('/class/hw/update', (req, res)=>{
+            let code = req.body.code, codex = req.body.uid, hcode = req.body.hcode, ne = req.body.new;
+            if(!number.test(code) || code=='' || code == undefined || codex == undefined || codex.length != 5 || hcode == undefined || !number.test(hcode) || ne == undefined) {
+                res.sendStatus(400);
+                return;
+            }
+            if(ne == '') {
+                res.status(400).send('blk_cont');
+                return;
+            }
+            let authed = function() {
+                return new Promise(function(resolve, reject) {
+                    cmanage.clinfDbQuery(`UPDATE h${code} SET content=$1 WHERE code=$2`, [ne, hcode], (err2)=>{
+                        if(err2) {
+                            reject('class_control: /class/hw/update update homework list error: '+err2);
+                            res.status(500).send('db');
+                        }
+                        else {
+                            res.sendStatus(200);
+                            resolve();
+                        }
+                    });
+                });
+            }
+            cmanage.clinfDbQuery(`SELECT uid FROM u${code} WHERE uid=$1`, [codex], (err1, res1)=>{
+                if(err1) {
+                    console.log('class_control: /class/hw/update select user auth query failure: '+err1);
+                    res.sendStatus(500);
+                    return
+                }
+                else if(res1.rowCount == 0) {
+                    res.status(401).send('auth');
+                    return
+                }
+                else if(res1.rowCount == 1) {
+                    authed().then(()=>{},(err)=>{
+                        console.log(err);
+                    });
+                }
+                else res.sendStatus(500);
+            });
+        });
     }
 }
